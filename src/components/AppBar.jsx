16e66b0cd3fee5245f55da24fa05b8f9742d5f8a
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2025-12-22 22:35:06 +0200

    Task 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,22 +1,31 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import theme from "../theme";
 import AppBarTab from "./AppBarTab";
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    alignItems: "center",
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.tabBar,
+  },
+  scroll: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab to="/">Repositories</AppBarTab>
-      <AppBarTab to="/signin">Sign In</AppBarTab>
+      <ScrollView
+        horizontal
+        contentContainerStyle={styles.scroll}
+        showsHorizontalScrollIndicator={false}
+      >
+        <AppBarTab to="/">Repositories</AppBarTab>
+        <AppBarTab to="/signin">Sign In</AppBarTab>
+      </ScrollView>
     </View>
   );
 };
